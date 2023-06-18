98f338bd71c6a2f82153665b6a8a61699a57ee3b
hkbhavin24 <43286829+hkbhavin24@users.noreply.github.com>
2023-06-18 16:36:14 +0530

    commit

--- app/src/main/java/com/example/datapirates/MainActivity.kt
+++ app/src/main/java/com/example/datapirates/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.datapirates
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.datapirates.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNavigationView.background = null
+        binding.bottomNavigationView.menu.getItem(2).isEnabled = false
     }
 }
