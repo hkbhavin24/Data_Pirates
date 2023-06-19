a3b4352b8b9de44518256d7084938a65ffc748bf
hkbhavin24 <43286829+hkbhavin24@users.noreply.github.com>
2023-06-19 15:36:33 +0530

    commit 2

--- app/src/main/java/com/example/datapirates/MainActivity.kt
+++ app/src/main/java/com/example/datapirates/MainActivity.kt
@@ -12,7 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.bottomNavigationView.background = null
-        binding.bottomNavigationView.menu.getItem(2).isEnabled = false
+
     }
 }
